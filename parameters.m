42e87163a1e9d041da3c6b4a963908702e41439f
Deepak7781 <deepak21082005@gmail.com>
2025-12-13 12:23:11 +0530

    add parametrs.m and Motor_Block.md

---
+++ parameters.m
@@ -0,0 +1,14 @@
+params = struct();
+
+params.m = 1.37;
+params.Ixx = 0.0219;
+params.Iyy = 0.0109;
+params.Izz = 0.0306;
+params.armLength = 0.18;
+params.propDia = 0.254;
+params.k_t = 1.3e-5;
+params.k_m = 1.1e-6;
+params.J = 7.5e-5;
+
+
+save('params.mat')
