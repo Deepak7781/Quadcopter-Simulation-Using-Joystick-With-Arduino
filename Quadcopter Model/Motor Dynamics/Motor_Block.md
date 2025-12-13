3a9e41252f2e723ca0c0041816b25ec46c2c6397
Deepak7781 <deepak21082005@gmail.com>
2025-12-13 12:42:10 +0530

    minor changes

---
+++ Quadcopter Model/Motor Dynamics/Motor_Block.md
@@ -0,0 +1,7 @@
+# Motor Dynamics
+
+So The vehicle we took for simulation is PX4 Iris.
+
+Let us model the motor dynamics for the quadcopter.
+
+![Motor Dynamics Block](Media/MotorBlock.png)
--- parameters.m
+++ parameters.m
@@ -1,14 +1,17 @@
-params = struct();
-
-params.m = 1.37;
-params.Ixx = 0.0219;
-params.Iyy = 0.0109;
-params.Izz = 0.0306;
-params.armLength = 0.18;
-params.propDia = 0.254;
-params.k_t = 1.3e-5;
-params.k_m = 1.1e-6;
-params.J = 7.5e-5;
 
 
+m = 1.37;
+Ixx = 0.0219;
+Iyy = 0.0109;
+Izz = 0.0306;
+armLength = 0.18;
+propDia = 0.254;
+k_t = 1.3e-5;
+k_m = 1.1e-6;
+J = 7.5e-5;
+Kp = 0.12;
+Ki = 14;
+R = 0.10;
+L = 4.0e-5;
+k_e = 0.02;
 save('params.mat')
